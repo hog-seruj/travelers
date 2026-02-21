9c8c809a5a40cc81e370e271e59208f9e56bf093
Iryna Yermak <1991irenka@gmail.com>
2026-02-21 10:54:49 +0000

    added type User and useAuthStore

---
+++ types/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  avatarUrl: string;
+  description: string;
+  articlesAmount: number;
+  savedArticles: string[];
+}
