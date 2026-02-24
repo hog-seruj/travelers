bf47e3a57d97c7dbf64e70d50c84793b0a8e41c8
Romanna <romannabric@gmail.com>
2026-02-24 22:25:03 +0100

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import style from './(site)/page.module.csst ';
+import style from './(site)/page.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect } from 'react';
