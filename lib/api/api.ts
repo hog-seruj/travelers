5c1289f90a4cf609d684f18cf57367fc231d4dde
Iryna Yermak <1991irenka@gmail.com>
2026-02-25 17:29:22 +0000

    fix build

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,11 +1,25 @@
 import axios from 'axios';
 
-const baseURLT = process.env.NEXT_PUBLIC_SERVER_API_URL + '/api';
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! використовується у тих, хто ще робить запити напряму на наш бекенд !!
+// const baseURL = 'http://localhost:3000/api';
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// export const nextServer = axios.create({
+export const api = axios.create({
+  baseURL: baseURL,
+  // withCredentials: true,
+});
+
+// axios Next Server !!!! Використовують ті, що переробляє через Next Server !!!!!!!!!!!!!!
+const baseURLT = process.env.NEXT_PUBLIC_SERVER_URL + '/api';
 
 export const nextServer = axios.create({
   baseURL: baseURLT,
   withCredentials: true,
 });
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 export type CreateStoryResponse = {
   _id: string;
