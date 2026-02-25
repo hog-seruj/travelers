45bcd3194269d00b95c49da2894debaad4c891e8
Olha Loz <olyakarabyn@gmail.com>
2026-02-25 11:28:17 +0200

    silent-authentication-and-logout-logic

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,8 +1,7 @@
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// import { nextServer } from './api';
-
+import { nextServer } from './api';
 import { Category, Story } from '@/types/story';
-import { api } from './api';
+import { User } from '@/types/user';
+
 export type StoryListResponse = {
   page: number;
   perPage: number;
@@ -17,8 +16,7 @@ export const getStories = async (
   sort?: 'newest' | 'popular',
   category?: Category
 ) => {
-  // const res = await nextServer.get<StoryListResponse>('/stories', {
-  const res = await api.get<StoryListResponse>('/stories', {
+  const res = await nextServer.get<StoryListResponse>('/stories', {
     params: {
       page,
       perPage,
@@ -27,4 +25,25 @@ export const getStories = async (
     },
   });
   return res.data;
+};
+
+// silent authentication logic
+type CheckSessionRequest = {
+  success: boolean;
+};
+
+export const checkSession = async () => {
+  const res = await nextServer.post<CheckSessionRequest>('/auth/refresh');
+  return res.data.success;
+};
+
+// get me
+export const getMe = async () => {
+  const { data } = await nextServer.get<User>('/users/me');
+  return data;
+};
+
+//logout
+export const logout = async (): Promise<void> => {
+  await nextServer.post('/auth/logout');
 };
