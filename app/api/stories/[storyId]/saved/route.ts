dbc392aad6626bea932e8082769174e517cd4994
Olha Loz <olyakarabyn@gmail.com>
2026-02-28 19:21:53 +0200

    make changes

--- app/api/stories/[storyId]/saved/route.ts
+++ app/api/stories/[storyId]/saved/route.ts
@@ -1,36 +1,68 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { api } from '@/app/api/api';
+import { api, ApiError } from '@/app/api/api';
 import { cookies } from 'next/headers';
-import { logErrorResponse } from '@/app/api/_utils/utils';
-import { isAxiosError } from 'axios';
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
 export async function POST(request: NextRequest, { params }: Props) {
+  const cookieStore = await cookies();
   const { id } = await params;
 
   try {
-    const cookieStore = await cookies();
-    const res = await api.post(`/stories/${id}/saved`, {
+    const res = await api.post(`/stories/${id}/saved`, null, {
       headers: {
         Cookie: cookieStore.toString(),
       },
     });
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error)) {
-      logErrorResponse(error.response?.data);
-      return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status }
-      );
-    }
-    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: 500 }
+      {
+        error:
+          (error as ApiError).response?.data?.error ??
+          (error as ApiError).message,
+      },
+      { status: (error as ApiError).status }
     );
   }
 }
+
+// copy
+// import { NextRequest, NextResponse } from 'next/server';
+// import { api, ApiError } from '@/app/api/api';
+// import { cookies } from 'next/headers';
+// import { logErrorResponse } from '@/app/api/_utils/utils';
+// import { isAxiosError } from 'axios';
+
+// type Props = {
+//   params: Promise<{ id: string }>;
+// };
+
+// export async function POST(request: NextRequest, { params }: Props) {
+//   const { id } = await params;
+
+//   try {
+//     const cookieStore = await cookies();
+//     const res = await api.post(`/stories/${id}/saved`, {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+//     return NextResponse.json(res.data, { status: res.status });
+//   } catch (error) {
+//     if (isAxiosError(error)) {
+//       logErrorResponse(error.response?.data);
+//       return NextResponse.json(
+//         { error: error.message, response: error.response?.data },
+//         { status: error.status }
+//       );
+//     }
+//     logErrorResponse({ message: (error as Error).message });
+//     return NextResponse.json(
+//       { error: 'Internal Server Error' },
+//       { status: 500 }
+//     );
+//   }
+// }
