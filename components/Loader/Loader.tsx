0f7ba1d44d9765850ee69765fe6a555bd9bd5706
Nataliia Bybyk <145537362+NataliiaBybyk@users.noreply.github.com>
2026-03-01 19:17:01 +0100

    Update Loader.tsx

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,18 +1,18 @@
-// 'use client';
+'use client';
 
 
-// import Icon from '../Icon/Icon';
+import Icon from '../Icon/Icon';
 
-// const Loader = () => {
-//   return (
-//     <div className={css.spinnerContainer}>
-//       <div className={css.spinner}></div>
-//       <div className={css.spinnerText}>
-//         Подорожуй за мить...
-//         <Icon src="/logo.svg" useSprite={false} className={css.logoIcon} />
-//       </div>
-//     </div>
-//   );
-// };
+const Loader = () => {
+  return (
+    <div className={css.spinnerContainer}>
+      <div className={css.spinner}></div>
+      <div className={css.spinnerText}>
+        Подорожуй за мить...
+        <Icon src="/logo.svg" useSprite={false} className={css.logoIcon} />
+      </div>
+    </div>
+  );
+};
 
-// export default Loader;
+export default Loader;
