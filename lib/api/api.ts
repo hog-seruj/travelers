53a237189ac3eb0560219f902861d45990c10069
Sergiy Borodulin <hogseruj1@gmail.com>
2026-03-03 11:20:08 +0200

    Update api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! використовується у тих, хто ще робить запити напряму на наш бекенд !!
-const baseURL = 'http://localhost:3000/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // export const nextServer = axios.create({
